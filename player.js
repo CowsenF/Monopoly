62bc08466bb4c803017024dcb44e6edd11c0bfd0
lassecalvillo <91175800+lassecalvillo@users.noreply.github.com>
2022-03-01 11:33:32 +0100

    op til 4 spillere kan nu deltage. Dobbelt rul fungere ved at playerTurn ikke ændre sig

--- player.js
+++ player.js
@@ -1,9 +1,9 @@
 class Player{
-    constructor(){
+    constructor(playerNR){
         // positionID beskriver hvilket felt spilleren er/lander på. Ved at
         // hente feltets information kan spilleren placeres på feltets placering.
         // spillerens første placering er altid GO
-        this.positionID = 7;
+        this.positionID = 0;
         // currentCredit beskriver hvor mange penge spilleren har
         this.currentCredit = 15000;
         // playerStage beskriver spillerens stadie (fri, fængsel, død)
@@ -11,13 +11,29 @@ class Player{
         this.diceRoll1 = 0;
         this.diceRoll2 = 0;
         this.playerRoll = 0;
+        this.playerNR = playerNR;
+
+        if(playerNR == 0){
+            this.color = 'rgb(255,0,0)';
+        }
+        else if(playerNR == 1){
+            this.color = 'rgb(0,255,0)';
+        }
+
+        else if (playerNR == 2){
+            this.color = 'rgb(0,0,255)';
+        }
+
+        else if (playerNR == 3){
+            this.color ='rgb(255,255,0)';
+        }
 
     }
 
     
 // function for dice roll
     rollDice(){
-        // two dices that randomly roles 1,2,3,4,5 or 6.
+        // two dices that randomly roles 1, 2, 3, 4, 5 or 6.
         this.diceRoll1 = floor(random(1,7));
         this.diceRoll2 = floor(random(1,7));
 
@@ -32,61 +48,68 @@ class Player{
             this.positionID -= 40;
 
         }
+
+        print(this.diceRoll1);
+        print(this.diceRoll2);
         
+        
+    }
+
+    drawDice(){
+        // the drawn images depends of the rolls of each dice, and is shown on the board to view player roll
+        imageMode(CENTER);
+        // First dice
+        
+            if(diceRoll1 == 1){
+                image(dice1, width/2 - 30, height/2, 50, 50);
+            }
+            else if(diceRoll1 == 2){
+                image(dice2, width/2 - 30, height/2, 50, 50);
+            }
+            else if(diceRoll1 == 3){
+                image(dice3, width/2 - 30, height/2, 50, 50);
+            }
+            else if(diceRoll1 == 4){
+                image(dice4, width/2 - 30, height/2, 50, 50);
+            }
+            else if(diceRoll1 == 5){
+                image(dice5, width/2 - 30, height/2, 50, 50);
+            }
+            else if(diceRoll1 == 6){
+                image(dice6, width/2 - 30, height/2, 50, 50);
+            }
+            // Second dice
+            if(diceRoll2 == 1){
+                image(dice1, width/2 + 30, height/2, 50, 50);
+            }
+            else if(diceRoll2 == 2){
+                image(dice2, width/2 + 30, height/2, 50, 50);
+            }
+            else if(diceRoll2 == 3){
+                image(dice3, width/2 + 30, height/2, 50, 50);
+            }
+            else if(diceRoll2 == 4){
+                image(dice4, width/2 + 30, height/2, 50, 50);
+            }
+            else if(diceRoll2 == 5){
+                image(dice5, width/2 + 30, height/2, 50, 50);
+            }
+            else if(diceRoll2 == 6){
+                image(dice6, width/2 + 30, height/2, 50, 50);
+            }
     }
 
     draw(){
 
         // The drawn ellipse represents the player position on the map
-        fill(255);
+        fill(this.color);
         ellipse(board.spaceList[this.positionID].x, board.spaceList[this.positionID].y, 25);
 
-        // the drawn images depends of the rolls of each dice, and is shown on the board to view roll
-        imageMode(CENTER);
-        // First die
-
-        // ændring
-        if(this.diceRoll1 == 1){
-            image(dice1, width/2 - 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll1 == 2){
-            image(dice2, width/2 - 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll1 == 3){
-            image(dice3, width/2 - 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll1 == 4){
-            image(dice4, width/2 - 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll1 == 5){
-            image(dice5, width/2 - 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll1 == 6){
-            image(dice6, width/2 - 30, height/2, 50, 50);
-        }
-        // Second die
-        if(this.diceRoll2 == 1){
-            image(dice1, width/2 + 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll2 == 2){
-            image(dice2, width/2 + 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll2 == 3){
-            image(dice3, width/2 + 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll2 == 4){
-            image(dice4, width/2 + 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll2 == 5){
-            image(dice5, width/2 + 30, height/2, 50, 50);
-        }
-        else if(this.diceRoll2 == 6){
-            image(dice6, width/2 + 30, height/2, 50, 50);
-        }
+        
     }
 
     update(){
         this.draw();
-        
+        this.drawDice();
     }
 }
